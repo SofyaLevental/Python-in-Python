9d2db79a3f0b78530605216ff43a5205c15ee634
Sofya Levental <sofya.levental@gmail.com>
2019-08-10 23:15:20 +0200

    snake game with pygame under tutorial with food

--- Game.py
+++ Game.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 
@@ -6,44 +8,94 @@ class Game:
         pass
 
 
+class Cell:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+    def __eq__(self, cell):
+        return self.i == cell.i and self.j == cell.j
+
+    def __hash__(self):
+        return hash((self.i, self.j))
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, point):
+        return self.x == point.x and self.y == point.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class Vector:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+    def __eq__(self, vector):
+        return self.i == vector.i and self.j == vector.j
+
+    def __hash__(self):
+        return hash((self.i, self.j))
+
 class Cube:
-    def __init__(self, position, x_direction=1, y_direction=0, color=(255, 0, 0)):
+    def __init__(self, position, direction=Vector(0, 1), color=(255, 0, 0)):
         self.position = position
-        self.x_direction = x_direction
-        self.y_direction = y_direction
+        self.direction = direction
         self.color = color
 
-    def move(self, x_direction, y_direction):
-        self.x_direction = x_direction
-        self.y_direction = y_direction
-        self.position = (self.position[0] + x_direction, self.position[1] + y_direction)
+    def move(self, direction):
+        self.direction = direction
+        self.position = Cell(self.position.i + self.direction.i, self.position.j + self.direction.j)
 
-    def draw(self, window, eyes=False):
-        upper_left_corner = [self.position[0] * cell_width + 1, self.position[1] * cell_width + 1]
-        down_right_corner = [(self.position[0] + 1) * cell_width, (self.position[1] + 1) * cell_width]
-        rect = (self.position[0] * cell_width + 1, self.position[1] * cell_width + 1, cell_width - 1, cell_width - 1)
+    def draw(self, eyes=False):
+        i_position = self.position.i
+        j_position = self.position.j
+        rect = (i_position * cell_width + 1, j_position * cell_width + 1, cell_width - 1, cell_width - 1)
         pygame.draw.rect(window, self.color, rect)
         if eyes:
             half_cell_width = cell_width // 2
-            cube_center = (self.position[0]*cell_width+1+half_cell_width, self.position[1]*cell_width+1+half_cell_width)
+            cube_center = Point(
+                i_position * cell_width + 1 + half_cell_width,
+                j_position * cell_width + 1 + half_cell_width
+            )
             radius = 3
-            left_eye_middle = (cube_center[0]-6,cube_center[1])
-            right_eye_middle = (cube_center[0]+6,cube_center[1])
-            pygame.draw.circle(window, (0,0,0), left_eye_middle, radius)
-            pygame.draw.circle(window, (0,0,0), right_eye_middle, radius)
+            left_eye_middle = Point(cube_center.x - 6, cube_center.y)
+            right_eye_middle = Point(cube_center.x + 6, cube_center.y)
+            pygame.draw.circle(window, (0, 0, 0), (left_eye_middle.x, left_eye_middle.y), radius)
+            pygame.draw.circle(window, (0, 0, 0), (right_eye_middle.x, right_eye_middle.y), radius)
 
 
 class Snake:
-    body = []
-    turns = {}
 
     def __init__(self, color, position):
+        self.body = []
+        self.turns = {}
         self.color = color
         self.position = position
         self.head = Cube(position)
         self.body.append(self.head)
-        self.x_direction = 1
-        self.y_direction = 0
+        self.direction = Vector(1, 0)
+
+    def add_cube(self):
+        tail = self.body[-1]
+        tail_direction = tail.direction
+
+        if tail_direction.i == 1 and tail_direction.j == 0:
+            self.create_new_tail_cube(tail.position.i - 1, tail.position.j)
+        elif tail_direction.i == -1 and tail_direction.j == 0:
+            self.create_new_tail_cube(tail.position.i + 1, tail.position.j)
+        elif tail_direction.i == 0 and tail_direction.j == 1:
+            self.create_new_tail_cube(tail.position.i, tail.position.j - 1)
+        elif tail_direction.i == 0 and tail_direction.j == -1:
+            self.create_new_tail_cube(tail.position.i, tail.position.j + 1)
+
+    def create_new_tail_cube(self, i_grid_position, j_grid_position):
+        self.body.append(Cube(Cell(i_grid_position, j_grid_position), self.body[-1].direction))
 
     def move(self):
         first_cell = 0
@@ -56,73 +108,81 @@ class Snake:
             keys = pygame.key.get_pressed()
             for key in keys:
                 if keys[pygame.K_LEFT]:
-                    self.update_direction(-1, 0)
+                    self.update_direction(Vector(-1, 0))
                 elif keys[pygame.K_RIGHT]:
-                    self.update_direction(1, 0)
+                    self.update_direction(Vector(1, 0))
                 elif keys[pygame.K_UP]:
-                    self.update_direction(0, -1)
+                    self.update_direction(Vector(0, -1))
                 elif keys[pygame.K_DOWN]:
-                    self.update_direction(0, 1)
+                    self.update_direction(Vector(0, 1))
 
-        for i, cube in enumerate(self.body):
-            position = cube.position[:]
+        for index, cube in enumerate(self.body):
+            position = cube.position
             if position in self.turns:
                 turn = self.turns[position]
-                cube.move(turn[0], turn[1])
-                self.remove_tail_from_turns(i, position)
+                cube.move(turn)
+                self.remove_tail_from_turns(index, position)
             else:
-                if cube.x_direction == -1 and cube.position[0] == first_cell:
-                    cube.position = (last_cell, cube.position[1])
-                elif cube.x_direction == 1 and cube.position[0] == last_cell:
-                    cube.position = (first_cell, cube.position[1])
-                elif cube.y_direction == -1 and cube.position[1] == first_cell:
-                    cube.position = (cube.position[0], last_cell)
-                elif cube.y_direction == 1 and cube.position[1] == last_cell:
-                    cube.position = (cube.position[0], first_cell)
+                if cube.direction.i == -1 and cube.position.i == first_cell:
+                    cube.position = Cell(last_cell, cube.position.j)
+                elif cube.direction.i == 1 and cube.position.i == last_cell:
+                    cube.position = Cell(first_cell, cube.position.j)
+                elif cube.direction.j == -1 and cube.position.j == first_cell:
+                    cube.position = Cell(cube.position.i, last_cell)
+                elif cube.direction.j == 1 and cube.position.j == last_cell:
+                    cube.position = Cell(cube.position.i, first_cell)
                 else:
-                    cube.move(cube.x_direction, cube.y_direction)
+                    cube.move(cube.direction)
 
-    def draw(self, window):
-        for i, cube in enumerate(self.body):
-            if i == 0:
-                cube.draw(window, True)
+    def draw(self):
+        for index, cube in enumerate(self.body):
+            if index == 0:
+                cube.draw(True)
             else:
-                cube.draw(window)
+                cube.draw()
 
-    def remove_tail_from_turns(self, i, position):
-        if i == len(self.body) - 1:
+    def remove_tail_from_turns(self, index, position):
+        if index == len(self.body) - 1:
             self.turns.pop(position)
 
-    def update_direction(self, x_direction, y_direction):
-        self.x_direction = x_direction
-        self.y_direction = y_direction
-        self.turns[self.head.position[:]] = [self.x_direction, self.y_direction]
+    def update_direction(self, direction):
+        self.direction = direction
+        self.turns[self.head.position] = self.direction
 
 
 def main():
-    global width, cells, cell_width, python
+    global width, cells, cell_width, window, python, food
     width = 500
     cells = 20
     cell_width = width // cells
     window = pygame.display.set_mode((width, width))
-    python = Snake((255, 0, 0), (10, 10))
+    python = Snake((255, 0, 0), Cell(10, 10))
+    food = create_food()
     flag = True
     clock = pygame.time.Clock()
     while flag:
         pygame.time.delay(50)
         clock.tick(10)
         python.move()
-        redraw_window(window)
+        if python.body[0].position.i == food.position.i and python.body[0].position.j == food.position.j:
+            python.add_cube()
+            food = create_food()
+        redraw_window()
 
 
-def redraw_window(window):
+def create_food():
+    return Cube(create_random_food_position(), color=(0, 255, 0))
+
+
+def redraw_window():
     window.fill((0, 0, 0))
-    python.draw(window)
-    draw_grid(window)
+    python.draw()
+    food.draw()
+    draw_grid()
     pygame.display.update()
 
 
-def draw_grid(window):
+def draw_grid():
     x = 0
     y = 0
     for i in range(cells):
@@ -130,6 +190,26 @@ def draw_grid(window):
         y = y + cell_width
         pygame.draw.line(window, (255, 255, 255), (0, y), (width, y))
         pygame.draw.line(window, (255, 255, 255), (x, 0), (x, width))
+
+
+def create_random_food_position():
+    snake_body_cubes = python.body
+
+    while True:
+        i_food_position = random.randrange(cells)
+        j_food_position = random.randrange(cells)
+        if len(list(
+                filter(lambda cube: is_food_on_body_cube(cube, i_food_position, j_food_position),
+                       snake_body_cubes))) > 0:
+            continue
+        else:
+            break
+
+    return Cell(i_food_position, j_food_position)
+
+
+def is_food_on_body_cube(cube, i_food_position, j_food_position):
+    return cube.position.i == i_food_position and cube.position.j == j_food_position
 
 
 main()
