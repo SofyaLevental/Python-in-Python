8a46f710017946ac57e5bf327f0474aa9adf9ca4
Sofya Levental <sofya.levental@gmail.com>
2019-08-11 22:44:13 +0200

    add first test for cube class

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,2 +1,16 @@
 import unittest
 
+from samples.objects import Cube
+from samples.utils import Cell, Vector
+
+
+class TestCubeMethods(unittest.TestCase):
+
+    def test_move(self):
+        cube = Cube(Cell(10, 10))
+        cube.move(Vector(1, 0))
+        self.assertEqual(cube.position, Cell(11, 10))
+
+
+if __name__ == '__main__':
+    unittest.main()
