f0e7208629dae40a861ac3b11e44ff564535d064
Sofya Levental <sofya.levental@gmail.com>
2019-08-14 18:47:33 +0200

    refactoring: extract pygame methods to visualization

--- samples/game.py
+++ samples/game.py
@@ -1,38 +1,22 @@
-import tkinter
-from tkinter import messagebox
-
-import pygame
-
 from samples.objects import Snake
 from samples.publisher import Publisher
 from samples.subscriber import Subscriber
-from samples.utils import Cell, Vector, BLACK, WHITE
-
-
-def message_box(subject, content):
-    root = tkinter.Tk()
-    root.attributes("-topmost", True)
-    root.withdraw()
-    messagebox.showinfo(subject, content)
-    root.destroy()
+from samples.utils import Cell, Vector
+from samples.visualization import Window, Popup, Timer
 
 
 def main():
-    global width, cells, cell_width, window, python, food
-    width = 500
+    global python
     cells = 20
-    cell_width = width // cells
-    window = pygame.display.set_mode((width + 1, width + 1))
     python = Snake(Cell(10, 10))
     food = python.create_food(cells)
-    flag = True
-    clock = pygame.time.Clock()
+    timer = Timer()
     publisher = Publisher()
     Subscriber().subscribe_for_commands(on_message)
+    window = Window(500, cells)
 
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(6)
+    while True:
+        timer.delay()
         publisher.listen_to_keyboard_events()
         python.move(cells)
         if python.get_head().position.i == food.position.i and python.get_head().position.j == food.position.j:
@@ -41,12 +25,12 @@ def main():
             body = python.get_body()
             del body[-1]
             python.set_body(body)
-        redraw_window()
+        window.redraw_window(python, food)
 
         for index in range(len(python.get_body())):
             if python.get_body()[index].position in list(
                     map(lambda cube: cube.position, python.get_body()[index + 1:])):
-                message_box("You Lost!", "Your Score: " + str(len(python.get_body())))
+                Popup.show("You Lost!", "Your Score: " + str(len(python.get_body())))
                 python.reset(Cell(10, 10))
                 break
 
@@ -54,7 +38,7 @@ def main():
 def on_message(client, userdata, message):
     key = str(message.payload.decode("utf-8"))
     if key == 'Q':
-        pygame.quit()
+        Window.quit_game()
     else:
         switcher = {
             'L': Vector(-1, 0),
@@ -63,36 +47,6 @@ def on_message(client, userdata, message):
             'D': Vector(0, 1)
         }
         python.update_direction(switcher.get(key))
-
-
-def redraw_window():
-    window.fill((0, 0, 0))
-    python.draw(cell_width, draw_rect, draw_circle)
-    food.draw(cell_width, draw_rect)
-    draw_grid()
-    pygame.display.update()
-
-
-def draw_rect(rect, color):
-    pygame.draw.rect(window, color, rect)
-
-
-def draw_circle(eye_middle, radius):
-    pygame.draw.circle(window, BLACK, (eye_middle.x, eye_middle.y), radius)
-
-
-def draw_grid():
-    x = 0
-    y = 0
-    for i in range(cells + 1):
-        draw_line((0, y), (width, y))
-        draw_line((x, 0), (x, width))
-        x = x + cell_width
-        y = y + cell_width
-
-
-def draw_line(begin, end):
-    pygame.draw.line(window, WHITE, begin, end)
 
 
 main()
---
+++ samples/visualization.py
@@ -0,0 +1,62 @@
+import tkinter
+from tkinter import messagebox
+
+import pygame
+
+from samples.utils import BLACK, WHITE
+
+
+class Timer:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+
+    def delay(self):
+        pygame.time.delay(50)
+        self.clock.tick(6)
+
+
+class Popup:
+    @staticmethod
+    def show(subject, content):
+        root = tkinter.Tk()
+        root.attributes("-topmost", True)
+        root.withdraw()
+        messagebox.showinfo(subject, content)
+        root.destroy()
+
+
+class Window:
+    def __init__(self, width, cells):
+        self.width = width
+        self.cells = cells
+        self.cell_width = self.width // self.cells
+        self.window = pygame.display.set_mode((width + 1, width + 1))
+
+    @staticmethod
+    def quit_game():
+        pygame.quit()
+
+    def redraw_window(self, python, food):
+        self.window.fill(BLACK)
+        python.draw(self.cell_width, self.draw_rect, self.draw_circle)
+        food.draw(self.cell_width, self.draw_rect)
+        self.draw_grid()
+        pygame.display.update()
+
+    def draw_rect(self, rect, color):
+        pygame.draw.rect(self.window, color, rect)
+
+    def draw_circle(self, eye_middle, radius):
+        pygame.draw.circle(self.window, BLACK, (eye_middle.x, eye_middle.y), radius)
+
+    def draw_grid(self):
+        x = 0
+        y = 0
+        for i in range(self.cells + 1):
+            self.draw_line((0, y), (self.width, y))
+            self.draw_line((x, 0), (x, self.width))
+            x = x + self.cell_width
+            y = y + self.cell_width
+
+    def draw_line(self, begin, end):
+        pygame.draw.line(self.window, WHITE, begin, end)
