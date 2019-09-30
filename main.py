9c99b0ddff9bb526911a76c953b165c7c275b31a
Sofya Levental <sofya.levental@gmail.com>
2019-09-30 21:54:30 +0200

    make game runnable from console

---
+++ main.py
@@ -0,0 +1,4 @@
+from samples.game import Game
+
+if __name__ == '__main__':
+    Game.start();
--- samples/game.py
+++ samples/game.py
@@ -4,44 +4,43 @@ from samples.subscriber import Subscriber
 from samples.utils import Cell, Vector
 from samples.visualization import Window, Timer
 
-python = Snake(Cell(10, 10))
 
+class Game:
+    PYTHON = Snake(Cell(10, 10))
 
-def main():
-    cells = 20
-    food = python.create_food(cells)
-    timer = Timer()
-    Subscriber.subscribe_for_commands(on_message)
-    window = Window(500, cells)
+    @staticmethod
+    def start():
+        cells = 20
+        food = Game.PYTHON.create_food(cells)
+        timer = Timer()
+        Subscriber.subscribe_for_commands(Game.on_message)
+        window = Window(500, cells)
 
-    while True:
-        timer.delay(50)
-        Publisher.listen_to_keyboard_events()
-        python.move(cells)
-        if python.has_collision():
-            window.show_message("You Lost!", "Your Score is: " + python.get_score())
-            timer.delay(1200)
-            python.reset(Cell(10, 10))
-        else:
-            if python.get_head().is_on_cube(food.get_position()):
-                food = python.create_food(cells)
+        while True:
+            timer.delay(50)
+            Publisher.listen_to_keyboard_events()
+            Game.PYTHON.move(cells)
+            if Game.PYTHON.has_collision():
+                window.show_message("You Lost!", "Your Score is: " + Game.PYTHON.get_score())
+                timer.delay(1200)
+                Game.PYTHON.reset(Cell(10, 10))
             else:
-                python.remove_tail()
-            window.redraw_window(python, food)
+                if Game.PYTHON.get_head().is_on_cube(food.get_position()):
+                    food = Game.PYTHON.create_food(cells)
+                else:
+                    Game.PYTHON.remove_tail()
+                window.redraw_window(Game.PYTHON, food)
 
-
-def on_message(client, userdata, message):
-    key = str(message.payload.decode("utf-8"))
-    if key == 'Q':
-        Window.quit_game()
-    else:
-        switcher = {
-            'L': Vector(-1, 0),
-            'R': Vector(1, 0),
-            'U': Vector(0, -1),
-            'D': Vector(0, 1)
-        }
-        python.update_direction(switcher.get(key))
-
-
-main()
+    @staticmethod
+    def on_message(client, userdata, message):
+        key = str(message.payload.decode("utf-8"))
+        if key == 'Q':
+            Window.quit_game()
+        else:
+            switcher = {
+                'L': Vector(-1, 0),
+                'R': Vector(1, 0),
+                'U': Vector(0, -1),
+                'D': Vector(0, 1)
+            }
+            Game.PYTHON.update_direction(switcher.get(key))
